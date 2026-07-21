ec025c1204767f80b3f9621f5ca0f9a4cc187036
ZuLu0890 <ayodelepraise2000@gmail.com>
2026-07-21 08:29:08 +0000

    feat(shared): export logger from shared package index
    Exposes createLogger through the @stellardao/shared barrel export
    so all packages can import structured logging with:
      import { createLogger } from '@stellardao/shared'

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,4 @@ export * from './types/index.js';
 export * from './types/governance.js';
 export * from './constants/index.js';
 export * from './env/index.js';
+export { createLogger } from './logger.js';
